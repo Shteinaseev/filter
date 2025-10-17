83ab6a4117fd17bab850a5905891fdcae3264df9
unknown <shteinaseev@gmail.com>
2025-10-17 11:52:37 +0200

    secont commit

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const listEl = document.querySelectorAll(".name-list > li")
+const listEl = document.querySelectorAll("#name-list > li")
 const searchBarEl = document.querySelector(".search-bar")
-console.log(listEl)
-console.log(searchBarEl)
-
 
 searchBarEl.addEventListener("keyup", () => {
     let searchTerm = searchBarEl.value.toLowerCase()
     listEl.forEach((item) => {
-        if (item.textContent.toLowerCase().includes(searchTerm)) {
-            item.style.display = "block"
+        if (!item.textContent.toLowerCase().includes(searchTerm)) {
+            item.style.opacity = "0"
+            item.style.fontSize = "0"
+        } else{
+            item.style.opacity = "1"
+            item.style.fontSize = "1.2rem"
         }
     })
 })
+
+const listEl2 = document.querySelectorAll("#prosek-list > li")
+const btnSortName = document.querySelector(".btn-sort-name")
+const btnSortAVG = document.querySelector(".btn-sort-average")
+
+const names = document.querySelectorAll(".name")
+const avg = document.querySelectorAll(".avg")
+
